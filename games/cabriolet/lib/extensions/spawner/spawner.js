5d1ed765579cf1b19fe84eafce70c538464638bc
Eugene Krevenets <hyzhak@ya.ru>
2013-04-17 13:39:33 +0200

    fix problems with getting scripts direct from github, throw raw.github.com

---
+++ games/cabriolet/lib/extensions/spawner/spawner.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+ * Project: darlingjs / GameEngine.
+ * Copyright (c) 2013, Eugene-Krevenets
+ */
+
+(function(darlingjs) {
+    var m = darlingjs.module('ngSpawner');
+    m.$c('ngSpawner', {
+        generate: null
+    });
+})(darlingjs);
