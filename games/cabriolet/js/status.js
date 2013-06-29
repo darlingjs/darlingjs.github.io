ebd9bde8e2c248dfcef4741f2b7d95ea4b4cc91e
Eugene Krevenets <hyzhak@ya.ru>
2013-06-29 22:55:11 +0200

    add status page

---
+++ games/cabriolet/js/status.js
@@ -0,0 +1,46 @@
+/**
+ * Project: darlingjs (GameEngine).
+ * Copyright (c) 2013, Eugene-Krevenets
+ */
+
+(function() {
+    'use strict';
+    game.controller('StatusCtrl', ['$scope', '$timeout'
+        ,function($scope, $timeout) {
+            // BORROWED from Mr Doob (mrdoob.com)
+
+            staticDatas();
+            dynanicDatas();
+
+            function dynanicDatas() {
+                $scope.document = document;
+                $scope.window = window;
+                $scope.landscape = window.innerWidth > window.innerHeight;
+                $timeout(dynanicDatas, 1000);
+            }
+
+            function staticDatas() {
+                $scope.webgl = ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )();
+            }
+
+            document.addEventListener('mousemove', function(event) {
+                $scope.$apply(function() {
+                    $scope.mouse = event;
+                });
+            });
+
+            document.addEventListener('touchmove', function(event) {
+                if (event.targetTouches && event.targetTouches.length > 0) {
+                    $scope.$apply(function() {
+                        $scope.touch = event.targetTouches[0];
+                    });
+                }
+                if (event.touches && event.touches.length > 0) {
+                    $scope.$apply(function() {
+                        $scope.touch = event.touches[0];
+                    });
+                }
+            });
+        }
+    ]);
+})();
